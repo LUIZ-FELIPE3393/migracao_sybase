4072a2c629cce377765b1a6bf56cf7a12df7888f
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-08-28 16:11:09 -0400

    modified:   api/con_mysql.py 	modified:   api/resultset.json

--- api/con_mysql.py
+++ api/con_mysql.py
@@ -23,11 +23,7 @@ def create_table( db_name, table_name ):
     )
 
     f = open("./api/resultset.json")
-
     data = json.load(f)
-
-    # print(data)
-
     mycursor = mydb.cursor()
 
     columns = []
@@ -53,13 +49,22 @@ def create_table( db_name, table_name ):
 
     mycursor.execute(f"CREATE TABLE {table_name} ({'%s' % ', '.join(map(str, columns))})")
 
-def primary_keys( db_name, table_name, column_names ):
+def insert_data( db_name, table_name ):
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
         password="5286",
         database=db_name
     )
+
+    f = open("./api/resultset.json")
+    data = json.load(f)
+    mycursor = mydb.cursor()
+
+    for i in data:
+        value_list = list(i.values())
+        print(f"CREATE TABLE {table_name} ({'''%s''' % ', '.join(map(str, value_list))})")
+        
 
 
 # Match for functions below
@@ -68,3 +73,5 @@ match sys.argv[2]:
         create_schema(sys.argv[3])
     case 'create_table':
         create_table(sys.argv[3], sys.argv[4])
+    case 'insert_data':
+        insert_data(sys.argv[3], sys.argv[4])
