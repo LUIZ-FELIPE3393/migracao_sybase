ed696b0e77fc9826fc5d87a02c8e2cb446457a5f
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-08-28 12:05:53 -0400

    Novas rotas

--- index.js
+++ index.js
@@ -10,6 +10,58 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./index.html"));
 });
 
+app.get("/sybase-tb/:database/:table", (req, res) => {
+    const pyPrc = spawn('python', ['./api/sybase.py', 'q_list_tables', [req.params.database, req.params.table], './api/resultset.json']);
+
+    pyPrc.stdout.on('data', (result) => {
+        console.log(result)
+
+        try {
+            readFile('./api/resultset.json', (err, data) => {
+                if(err) {
+                    console.error(err);
+                    throw err;
+                }
+                const resultParsed = JSON.parse(data?.toString());
+                res.send(resultParsed);
+            });
+            
+        } catch (error) {
+            console.error(error);
+        }
+    })
+
+    pyPrc.stderr.on('error', (error) => {
+        console.error(error);
+    }) 
+});
+
+app.get("/sybase-tb/:database", (req, res) => {
+    const pyPrc = spawn('python', ['./api/sybase.py', 'q_list_tables', req.params.database, './api/resultset.json']);
+
+    pyPrc.stdout.on('data', (result) => {
+        console.log(result)
+
+        try {
+            readFile('./api/resultset.json', (err, data) => {
+                if(err) {
+                    console.error(err);
+                    throw err;
+                }
+                const resultParsed = JSON.parse(data?.toString());
+                res.send(resultParsed);
+            });
+            
+        } catch (error) {
+            console.error(error);
+        }
+    })
+
+    pyPrc.stderr.on('error', (error) => {
+        console.error(error);
+    }) 
+})
+
 app.get("/sybase-db", (req, res) => {
     const pyPrc = spawn('python', ['./api/sybase.py', 'q_databases', '', './api/resultset.json']);
 
